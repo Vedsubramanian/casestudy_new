f3eb664b9ef98ed5b57487f4ff3661da694212ff
Vedsubramanian <52442368+Vedsubramanian@users.noreply.github.com>
2019-07-03 17:35:00 +0530

    Add files via upload

---
+++ src/test/java/runner/RunCukesTest.java
@@ -0,0 +1,12 @@
+package runner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features={"src/test/resources/signup.feature","src/test/resources/signin.feature","src/test/resources/search.feature","src/test/resources/addtocart.feature"},
+glue="Stepdefinition",
+plugin="json:target/cucumber.json")
+public class RunCukesTest {
+}
